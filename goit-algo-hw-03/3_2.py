04ddd246be158eec92b62f56b71eeafa1a3fbb7c
Experrt2 <experrt2@gmail.com>
2025-03-10 16:33:12 +0200

    change date format

--- goit-algo-hw-03/3_2.py
+++ goit-algo-hw-03/3_2.py
@@ -1,11 +1,13 @@
 import random
+import sys
 
 def get_numbers_ticket(min_value, max_value, quantity):
-    if min_value < 1 and max_value > 1000:
+    if (min_value < 1 and max_value > 1000) or (max_value-min_value < quantity):
         return []
 
     lottery_numbers = sorted(random.sample(range(min_value, max_value), quantity))
 
     return lottery_numbers
 
-print(get_numbers_ticket(1, 1000, 9))
+print(get_numbers_ticket(14, 3, 4))
+
